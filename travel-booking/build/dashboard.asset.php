1f5ea3fc07f3cf16f45963c7def101a0d9420ae2
Sean Murphy <sean@iamseanmurphy.com>
2025-03-27 14:17:11 +0000

    1.3.9

--- travel-booking/build/dashboard.asset.php
+++ travel-booking/build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b2b4cf9dd9e78127735c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '4400907d6fbc96a1bb48');
